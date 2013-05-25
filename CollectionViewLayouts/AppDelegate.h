822e7a73d8b2f3edb8515a7c3af280699798869f
Ramon Bartl <ramon_bartl@yahoo.de>
2013-05-25 12:26:27 +0200

    Initial Commit

---
+++ CollectionViewLayouts/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewLayouts
+//
+//  Created by Ramon Bartl on 25.05.13.
+//  Copyright (c) 2013 Ramon Bartl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewLayouts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewLayouts
+//
+//  Created by Ramon Bartl on 25.05.13.
+//  Copyright (c) 2013 Ramon Bartl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
